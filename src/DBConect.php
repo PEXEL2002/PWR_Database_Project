4e0efb85a60f6c0d119b91c80368a8ddedb56fd7
Bartlomiej Kuk <bartek.kuk321@gmail.com>
2024-11-19 22:32:25 +0100

    Refactor Database class to improve connection handling with stmt

--- src/DBConect.php
+++ src/DBConect.php
@@ -1,62 +1,33 @@
-<?php 
+<?php
 require_once 'config.php';
-class Database{
+
+class Database {
     /**
-    * Database connection properties
-    *    @param $conn - database connection variable
-    */
+     * Database connection property
+     * @var mysqli
+     */
     private $conn;
     /**
-     * Database connection
-     *   @return $conn - database connection
+     * Constructor - initializes database connection
      */
-    public function __construct(){
+    public function __construct() {
         $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        if($this->conn->connect_error){
+        if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
         }
     }
-    /** 
-     * Execute query
-     *  @param $query - query to be executed
-     *  @return $result - result of the query
-    */
-    public function execute($query){
-        return $this->conn->query($query);
-        
-    }
     /**
-     * Get array from query
-     * @param $query - query to be executed
-     * @return $data - array of data from the query
+     * Get the database connection
+     * @return mysqli
      */
-    public function getArray($query){
-        $result = $this->conn->query($query);
-        $data = [];
-        if($result->num_rows > 0){
-            while($row = $result->fetch_assoc()){
-                $data[] = $row;
-            }
-        }
-        return $data;
-    }
-    /**
-     * Get number of rows from query
-     * @param $query - query to be executed
-     * @return $result->num_rows - number of rows from the query
-     */
-    public function getNumRows($query){
-        $result = $this->conn->query($query);
-        return $result->num_rows;
+    public function getConnection() {
+        return $this->conn;
     }
     /**
      * Close database connection
-     * @return void
      */
-    public function __destruct(){
+    public function __destruct() {
         $this->conn->close();
     }
-
 }
-
 ?>
