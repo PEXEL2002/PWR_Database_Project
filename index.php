cf4a0b658af123f736396e124d19631b94e3d681
Bartlomiej Kuk <bartek.kuk321@gmail.com>
2024-12-18 00:19:35 +0100

    Add database backup functionality before redirecting to main page

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
-    header("Location: public/main.php");
-    exit;
+require_once __DIR__ . '/src/config.php';
+function createDatabaseBackup() {
+    $host = DB_HOST;
+    $user = DB_USER;
+    $password = DB_PASS;
+    $dbName = DB_NAME;
+    $backupDir = __DIR__ . '/database/backups';
+    if (!file_exists($backupDir)) {
+        mkdir($backupDir, 0777, true);
+    }
+    $date = date('Y-m-d');
+    $backupFile = "{$backupDir}/backup_{$date}.sql";
+    $command = "mysqldump -h {$host} -u {$user} -p{$password} {$dbName} > \"{$backupFile}\"";
+    exec($command);
+}
+createDatabaseBackup();
+header("Location: public/main.php");
+exit;
 ?>
