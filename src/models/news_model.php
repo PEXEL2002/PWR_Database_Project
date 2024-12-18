5a4ac2694401a3ed1cf0e7e69007c39ac9d3dc58
Bartlomiej Kuk <bartek.kuk321@gmail.com>
2024-12-18 01:04:34 +0100

    Refactor News model: update addNews method to accept parameters and add deleteNews method

--- src/models/news_model.php
+++ src/models/news_model.php
@@ -1,22 +1,22 @@
 <?php
-require_once  realpath(__DIR__ . '/../DBConect.php');
-class News{
+require_once realpath(__DIR__ . '/../DBConect.php');
+
+class News {
     private $dbConect;
     private $db;
-    public function  __construct(){
+
+    public function __construct() {
         $this->dbConect = new Database();
         $this->db = $this->dbConect->getConnection();
     }
-    
-    public function getNews(){
-        $conn = $this->db;
 
+    public function getNews() {
+        $conn = $this->db;
         // Sprawdź połączenie
         if (!$conn) {
             die("Połączenie z bazą danych jest zamknięte lub nie istnieje.");
         }
-    
-        $query = "SELECT news.*,users.U_name FROM news JOIN USERs ON news.N_creator = users.U_id";
+        $query = "SELECT news.*, users.U_name FROM news JOIN users ON news.N_creator = users.U_id";
         $result = $conn->query($query);
         $news = [];
         if ($result && $result->num_rows > 0) {
@@ -26,20 +26,21 @@ class News{
         }
         return $news;
     }
-
-    public function addNews(){
-        if(!isset($_POST['title']) || !isset($_POST['content'])){
-            return false;
-        }
-        if(!isset($_POST['image'])){
-            $_POST['image'] = '';
-        }
+    public function addNews($title, $content, $photoPath, $creatorId) {
         $conn = $this->db;
-        $query = "INSERT INTO news (N_title, N_content, N_image) VALUES (?, ?, ?)";
+        $query = "INSERT INTO news (N_title, N_content, N_photo, N_creator) VALUES (?, ?, ?, ?)";
         $stmt = $conn->prepare($query);
-        $stmt->bind_param("sss", $_POST['title'], $_POST['content'], $_POST['image']);
+        if (!$stmt) {
+            throw new Exception("Błąd przygotowania zapytania: " . $conn->error);
+        }
+        $stmt->bind_param("sssi", $title, $content, $photoPath, $creatorId);
         $stmt->execute();
-        return $stmt->affected_rows;
+    }
+    public function deleteNews($newsId) {
+        $query = "DELETE FROM news WHERE N_id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("i", $newsId);
+        $stmt->execute();
     }
 }
 ?>
